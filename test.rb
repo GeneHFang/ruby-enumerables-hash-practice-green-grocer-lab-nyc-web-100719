27b7879dd5f78e908696fc41257b043eb4d6decb
Gene H Li <ghl234@nyu.edu>
2019-09-25 21:38:35 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -30,12 +30,85 @@ def coupons
 	]
 end
 
+def consolidate_cart(cart)
+  # code here
+  newHash = Hash.new
+  
+  
+  cart.map { |n|
+  
+    m = n.keys[0]
+    
+    
+    if (newHash.key?(m))
+      newHash[m][:count] = newHash[m][:count] + 1
+    else
+      
+        
+      newHash[m] = {
+        price: n[m][:price],
+        clearance: n[m][:clearance],
+        count: 1
+        
+      }
+    end
+  
+  }
+  
+  newHash
+end
+
+
+
+
+    
+
+def apply_coupons(cart, coupons)
+  # code here
+  
+  newHash = Hash.new
+  i = 0
+  coupons.map{ |n|
+    
+    if (cart.key?(n[:item]))
+      cart[n[:item]][:count] -= n[:num]
+      #clearance = cart[n[:item]][:clearance]
+     clearance = cart[n[:item]][:clearance]
+      string = n[:item].concat(" W/COUPON")
+      price = n[:cost] / n[:num]
+      count = n[:num]
+      
+      #delete if quantity is 0
+      #if (cart[n[:item]][:count] < 1)
+       # cart.delete(n[:item])
+      #end
+      
+      if (cart.key?(string))
+        price = cart[string][:price] + price
+        
+        count = cart[string][:num] + count
+        cart[string][:price] = price
+        cart[string][:clearance] = clearance,
+        cart[string][:count] =  count
+      else
+        cart[string] = {
+          price: price,
+          clearance: clearance,
+          count: count
+        }
+      end
+    end
+    
+  }
+  
+  cart
+end
 
 avocado = find_item("AVOCADO")
 coupon = find_coupon("AVOCADO")
 consol_cart = consolidate_cart([avocado, avocado, avocado, avocado, avocado])
 two_coupon_result = apply_coupons(consol_cart, [coupon, coupon])
-
+puts two_coupon_result
 
 
 
