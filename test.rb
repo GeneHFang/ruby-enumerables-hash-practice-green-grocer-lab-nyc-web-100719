50ad5a1d8bb4806860cff9362c3d4561d77c49ab
Gene H Li <ghl234@nyu.edu>
2019-09-25 21:50:13 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -74,7 +74,8 @@ def apply_coupons(cart, coupons)
       cart[n[:item]][:count] -= n[:num]
       #clearance = cart[n[:item]][:clearance]
      clearance = cart[n[:item]][:clearance]
-      string = n[:item].concat(" W/COUPON")
+      string = n[:item]
+      string += " W/COUPON"
       price = n[:cost] / n[:num]
       count = n[:num]
       
