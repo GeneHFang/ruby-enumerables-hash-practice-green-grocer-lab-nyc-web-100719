9088e9d4474fd358108327781fb66e84eed5be63
Gene H Li <ghl234@nyu.edu>
2019-09-25 20:16:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,42 @@
 def consolidate_cart(cart)
   # code here
+  newHash = Hash.new
+  
+  
+  cart.map { |n|
+  
+    m = n.keys[0]
+    
+    
+    if (newHash.key?(m))
+      newHash[m][:count] = newHash[m][:count] + 1
+    else
+      
+        
+      newHash[m] = {
+        price: n[m][:price],
+        clearance: n[m][:clearance],
+        count: 1
+        
+      }
+    end
+  
+  }
+  
+  newHash
 end
+
+
+
+
+    
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.map{ |n| 
+    n[:items]
+    
+  }
 end
 
 def apply_clearance(cart)
