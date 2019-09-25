1229143ac06ff56b0f6e47ab94406866adb59bfb
Gene H Li <ghl234@nyu.edu>
2019-09-25 21:57:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,52 +25,27 @@ def consolidate_cart(cart)
   
   newHash
 end
-
-
-
-
     
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupons) 
   
-  newHash = Hash.new
-  i = 0
-  coupons.map{ |n|
+  coupons.each { |n| 
+    n.each do |attribute, value| 
+      name = coupon[:item] 
     
-    if (cart.key?(n[:item]))
-      cart[n[:item]][:count] -= n[:num]
-      #clearance = cart[n[:item]][:clearance]
-     clearance = cart[n[:item]][:clearance]
-      temp = n[:item]
-      string = temp.concat(" W/COUPON")
-      price = n[:cost] / n[:num]
-      count = n[:num]
-      
-      #delete if quantity is 0
-      #if (cart[n[:item]][:count] < 1)
-       # cart.delete(n[:item])
-      #end
-      
-      if (cart.key?(string))
-        price = cart[string][:price] + price
-        
-        count = cart[string][:num] + count
-        cart[string][:price] = price
-        cart[string][:clearance] = clearance,
-        cart[string][:count] =  count
-      else
-        cart[string] = {
-          price: price,
-          clearance: clearance,
-          count: count
-        }
-      end
-    end
-    
-  }
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
   
-  cart
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+}
+  cart 
 end
 
 def apply_clearance(cart)
