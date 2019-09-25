899a871a828a5d685bb1d6bc3d46c90fc8a04510
Gene H Li <ghl234@nyu.edu>
2019-09-25 21:02:29 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -24,20 +24,8 @@ def coupons
 	]
 end
 
-[
-		{"AVOCADO" => {:price => 3.00, :clearance => true}},
-		{"KALE" => {:price => 3.00, :clearance => false}},
-		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-		{"ALMONDS" => {:price => 9.00, :clearance => false}},
-		{"TEMPEH" => {:price => 3.00, :clearance => true}},
-		{"CHEESE" => {:price => 6.50, :clearance => false}},
-		{"BEER" => {:price => 13.00, :clearance => false}},
-		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-		{"BEETS" => {:price => 2.50, :clearance => false}}
-	]
-
 puts "begin this shit\n"
-puts items["AVOCADO"][:clearance]
+puts items[0][coupons[0][:items]][:clearance]
 puts "\nend\n\n\n\n"
 
 def generate_cart
