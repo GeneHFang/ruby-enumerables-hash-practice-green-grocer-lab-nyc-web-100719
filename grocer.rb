f5870d887a539b95c7a1db3e505d87da6061ad19
Gene H Li <ghl234@nyu.edu>
2019-09-25 21:11:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def apply_coupons(cart, coupons)
       
       cart[string] = {
         price: price,
-        clearance: cart[n[:item]][:clearance],
+        #clearance: cart[n[:item]][:clearance],
         count: n[:num]
       }
     end
